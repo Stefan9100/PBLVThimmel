12798f87ace10e389c7b63bc3fce8c3f14df1606
Jasmin Vuong <vuong@hm.edu>
2024-02-07 17:15:43 +0100

    "adjust latest charging time"

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import warnings
-import datetime
+from datetime import datetime, timedelta
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -14,6 +14,7 @@ pv_powerData = pd.read_excel(excel_file, sheet_name="Erzeugung", usecols=pv_powe
 car_travelData = pd.read_excel(excel_file, sheet_name="Autofahrplan")
 car_travelData['Spaetester-Lade-Tag'] = ''
 car_travelData['Spaeteste-Lade-Uhrzeit'] = ''
+car_travelData.loc[car_travelData['Fahrzeug'] == 1, 'Spaeteste-Lade-Uhrzeit'] = '23:00'  
 car_masterData = {'Id': [1, 2],
                   'Modell': ['BMW i4', 'Tesla Model 3'],
                   'Verbrauch in kWh/100km': [60, 80],
@@ -29,23 +30,71 @@ car_travelData = pd.DataFrame(car_travelData)
 
 
 car_travelData.sort_values(by=["Ankunft-Tag", "Ankunft-Uhrzeit"], inplace=True)
-# print Data test
-#print(pv_powerData.to_string())
-print(car_travelData.to_string())
 
-for index, powerDataEntry in pv_powerData.iterrows():
-    if powerDataEntry["Erzeugung in Watt (W)"] > 0:
-        for index, travelDataEntry in car_travelData.iterrows():
-            arrival_time = travelDataEntry["Ankunft-Uhrzeit"]
-            departure_time = travelDataEntry["Abfahrt-Uhrzeit"]
-            current_time = powerDataEntry["Uhrzeit"]
-            if (travelDataEntry["Ankunft-Tag"] <= powerDataEntry["Tag"]
-                and arrival_time <= current_time
-                and travelDataEntry["Abfahrt-Tag"] >= powerDataEntry["Tag"]
-                and departure_time >= current_time):
-                carId = travelDataEntry["Fahrzeug"]
-                car_masterData.at[carId, "IsThere"] = True
-            else:
-                carId = travelDataEntry["Fahrzeug"]
-                car_masterData.at[carId, "IsThere"] = False
-        print(car_masterData[["Modell", "IsThere"]])
+# angenommen spätester Ladezeitpunkt gegeben
+# verschiebt die spätere Lade Uhrzeit, wenn davor bereits geladen wurde
+
+def adjust_charging_schedule_based_on_remaining_load(car_travelData, car_id, loaded_kwh):
+    # Wie viele kWh in 5 Minuten geladen werden können
+    kwh_per_interval = (4 / 60) * 5  
+    
+    # spezifisches Auto
+    car_data = car_travelData[car_travelData['Fahrzeug'] == car_id]
+    
+    for index, row in car_data.iterrows():
+        # verbleibende erforderliche Ladung
+        remaining_kwh = row['Notwendige Ladung'] - loaded_kwh
+        
+        # Sicherstellen, dass verbleibende Ladung nicht negativ
+        remaining_kwh = max(remaining_kwh, 0)
+        
+        # benötigte Intervalle, für verbleibende benötigte Ladung
+        intervals_needed = remaining_kwh / kwh_per_interval
+        
+        # neue späteste Ladezeit basierend auf verbleibende Ladung
+        latest_charging_time_str = f"{row['Spaetester-Lade-Tag']} {row['Spaeteste-Lade-Uhrzeit']}"
+        latest_charging_time = datetime.strptime(latest_charging_time_str, '%d %H:%M')
+        new_latest_charging_time = latest_charging_time + timedelta(minutes=intervals_needed * 5)
+        
+        # Update den DataFrame mit der neuen spätesten Ladezeit
+        car_travelData.at[index, 'Spaeteste-Lade-Uhrzeit'] = new_latest_charging_time.strftime('%H:%M')
+    
+    return car_travelData
+
+# Test
+adjusted_car_travelData = adjust_charging_schedule_based_on_remaining_load(car_travelData, 1, 10)
+print(adjusted_car_travelData[['Fahrzeug', 'Ankunft-Tag', 'Ankunft-Uhrzeit', 'Spaetester-Lade-Tag', 'Spaeteste-Lade-Uhrzeit']])
+
+
+# alte Version Abzug der geladenen kW im 5 Minuten-Takt korrekt jedoch, kein Verschieben der späteren Lade Uhrzeit, da dafür eine Start Ladezeit gegeben sein muss
+
+# def adjust_charging_schedule(car_travelData, car_id, additional_kwh):
+  
+#     # Wie viele kWh in 5 Minuten geladen werden können
+#     kwh_per_interval = (4/ 60) * 5
+    
+#     # spezifisches Auto
+#     car_data = car_travelData[car_travelData['Fahrzeug'] == car_id]
+
+#     for index, row in car_data.iterrows():
+#         # benötigte Intervalle, um  zusätzlichen kWs zu laden
+#         intervals_needed = additional_kwh / kwh_per_interval
+        
+#         # neue späteste Ladezeit
+#         latest_charging_time_str = f"{row['Spaetester-Lade-Tag']} {row['Spaeteste-Lade-Uhrzeit']}"
+#         latest_charging_time = datetime.strptime(latest_charging_time_str, '%d %H:%M')
+#         new_latest_charging_time = latest_charging_time + timedelta(minutes=intervals_needed * 5)
+        
+#         # Update DataFrame mit neuen spätesten Ladezeit
+#         car_travelData.at[index, 'Spaeteste-Lade-Uhrzeit'] = new_latest_charging_time.strftime('%H:%M')
+        
+#         # DataFrame, für Ladevorgang im 5-Minuten-Takt 
+#         charging_intervals = pd.DataFrame({
+#             'Zeit': [latest_charging_time + timedelta(minutes=5*i) for i in range(int(intervals_needed))],
+#             'Geladene kWh': [kwh_per_interval * (i+1) for i in range(int(intervals_needed))]
+#         })
+        
+#         print(f"Ladevorgang für Fahrzeug {car_id} im 5-Minuten-Takt:")
+#         print(charging_intervals.to_string(index=False))
+
+#     return car_travelData
